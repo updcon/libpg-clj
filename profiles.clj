49e516711d8bb6a21bcde405a00d90ed11f89209
MelKori <mieldo@gmail.com>
2018-12-24 21:16:34 +0200

    update structure

--- profiles.clj
+++ profiles.clj
@@ -4,14 +4,16 @@
             :deploy-repositories [["private-jars-scp" {:url              "scp://local.repo/home/clojar/data/dev_repo/"
                                                        :username         "clojar"
                                                        :private-key-file :env/clojure_ssh_key}]]}
- :provided {:dependencies [[org.clojure/clojure "1.9.0"]
+ :provided {:dependencies [[org.clojure/clojure "1.10.0"]
                            ;; Logger
-                           [ch.qos.logback/logback-classic "1.2.3"
-                            :exclusions [org.slf4j/slf4j-api]]
+                           [com.fzakaria/slf4j-timbre "0.3.12"]
+                           [org.slf4j/slf4j-api "1.7.25"]
+                           [org.slf4j/log4j-over-slf4j "1.7.25"]
                            [org.slf4j/jul-to-slf4j "1.7.25"]
                            [org.slf4j/jcl-over-slf4j "1.7.25"]
-                           [org.slf4j/log4j-over-slf4j "1.7.25"]
-                           [org.clojure/tools.logging "0.4.0"]]}
+                           [ch.qos.logback/logback-classic "1.2.3"
+                            :exclusions [org.slf4j/slf4j-api]]
+                           [com.taoensso/timbre "4.10.0"]]}
  :uberjar  {:aot :all :jvm-opts #=(eval
                                     (concat ["-Xmx1G"]
                                       (let [version (System/getProperty "java.version")
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.dkdhub/libpg-clj "0.0.1"
+(defproject com.dkdhub/libpg-clj "0.0.2"
   :description "DKD commons for PostgreSQL"
   :url "https://dkdhub.com"
   :license {:name "Proprietary"
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/java.jdbc "0.7.8"]
                  ;; PostgreSQL
                  [org.postgresql/postgresql "42.2.5"]
-                 [nilenso/honeysql-postgres "0.2.4"]
+                 [nilenso/honeysql-postgres "0.2.5"]
                  [com.mchange/c3p0 "0.9.5.2"]
                  ;; Data helpers
                  [danlentz/clj-uuid "0.1.7"]
