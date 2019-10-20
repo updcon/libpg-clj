7367c064ff537735dd39e8c42a1263c12faa65c0
MelKori <mieldo@gmail.com>
2019-10-20 22:36:44 +0300

    update project dependencies

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject com.dkdhub/libpg-clj "0.0.3"
+(defproject com.dkdhub/libpg-clj "0.0.4"
   :description "DKD commons for PostgreSQL"
   :url "https://dkdhub.com"
   :license {:name "Proprietary"
             :url  "https://dkdhub.com/licenses/base.html"}
-  :dependencies [[org.clojure/java.jdbc "0.7.9"]
+  :dependencies [[org.clojure/java.jdbc "0.7.10"]
                  ;; PostgreSQL
-                 [org.postgresql/postgresql "42.2.6"]
+                 [org.postgresql/postgresql "42.2.8"]
                  [nilenso/honeysql-postgres "0.2.6"]
                  [com.mchange/c3p0 "0.9.5.4"]
                  ;; Data helpers
                  [danlentz/clj-uuid "0.1.9"]
                  [cheshire "5.9.0"]
-                 [joda-time "2.10.3"]])
+                 [joda-time "2.10.4"]])
 
 (cemerick.pomegranate.aether/register-wagon-factory!
   "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
