9c3d2db5095d53afa1ae93a1857be8739a6f9711
MelKori <mieldo@gmail.com>
2022-01-17 00:30:56 +0200

    update profiles

--- profiles.clj
+++ profiles.clj
@@ -1,16 +1,16 @@
-{:dev      {:plugins             [[org.apache.maven.wagon/wagon-ssh-external "3.4.2"]
-                                  [org.apache.maven.wagon/wagon-http-lightweight "3.4.2"]]
+{:dev      {:plugins             [[org.apache.maven.wagon/wagon-ssh-external "3.5.1"]
+                                  [org.apache.maven.wagon/wagon-http-lightweight "3.5.1"]]
             :deploy-repositories [["private-jars-scp" {:url              "scp://local.repo/home/clojar/data/dev_repo/"
                                                        :username         "clojar"
                                                        :private-key-file :env/clojure_ssh_key}]]}
- :provided {:dependencies [[org.clojure/clojure "1.10.1"]
+ :provided {:dependencies [[org.clojure/clojure "1.10.3"]
                            ;; Logger
-                           [com.fzakaria/slf4j-timbre "0.3.20"]
-                           [org.slf4j/slf4j-api "1.7.30"]
-                           [org.slf4j/log4j-over-slf4j "1.7.30"]
-                           [org.slf4j/jul-to-slf4j "1.7.30"]
-                           [org.slf4j/jcl-over-slf4j "1.7.30"]
-                           [ch.qos.logback/logback-classic "1.2.3"
+                           [com.fzakaria/slf4j-timbre "0.3.21"]
+                           [org.slf4j/slf4j-api "1.7.33"]
+                           [org.slf4j/log4j-over-slf4j "1.7.33"]
+                           [org.slf4j/jul-to-slf4j "1.7.33"]
+                           [org.slf4j/jcl-over-slf4j "1.7.33"]
+                           [ch.qos.logback/logback-classic "1.2.10"
                             :exclusions [org.slf4j/slf4j-api]]
-                           [com.taoensso/timbre "5.1.0"]]}
+                           [com.taoensso/timbre "5.1.2"]]}
  :jar      {:aot :all}}
