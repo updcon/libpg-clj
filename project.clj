52a17c03109314d5d1101cd806ec227dcd3f3998
MelKori <mieldo@gmail.com>
2022-01-17 00:28:19 +0200

    update main deps

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://dkdhub.com"
   :license {:name "Proprietary"
             :url  "https://dkdhub.com/licenses/base.html"}
-  :dependencies [[org.clojure/java.jdbc "0.7.11"]
+  :dependencies [[org.clojure/java.jdbc "0.7.12"]
                  ;; PostgreSQL
-                 [org.postgresql/postgresql "42.2.18"]
-                 [nilenso/honeysql-postgres "0.2.6"]
+                 [org.postgresql/postgresql "42.3.1"]
+                 [nilenso/honeysql-postgres "0.4.112"]
                  [com.mchange/c3p0 "0.9.5.5"]
                  ;; Data helpers
                  [danlentz/clj-uuid "0.1.9"]
-                 [cheshire "5.10.0"]
-                 [joda-time "2.10.8"]])
+                 [cheshire "5.10.1"]
+                 [joda-time "2.10.13"]])
 
 (cemerick.pomegranate.aether/register-wagon-factory!
   "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
