2d9656db48cbaa6ac3d48235e0fb0eed2507cee6
MelKori <mieldo@gmail.com>
2022-08-29 23:27:07 +0300

    change library group

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject com.dkdhub/libpg-clj "0.1.0"
+(defproject ai.z7/libpg-clj "0.1.0"
   :description "DKD commons for PostgreSQL in Clojure"
   :url "https://dkdhub.com"
-  :license {:name "MIT"}
+  :license {:name "MIT"
+            :url "https://github.com/updcon/libpg-clj"}
   :dependencies [[org.clojure/java.jdbc "0.7.12"]
                  ;; PostgreSQL
                  [org.postgresql/postgresql "42.5.0"]
