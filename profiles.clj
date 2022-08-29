a12591879448b982fc64891dd39b586177ff334b
MelKori <mieldo@gmail.com>
2022-08-29 23:21:12 +0300

    update development deps

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,12 @@
 {:dev      {:plugins []}
- :provided {:dependencies [[org.clojure/clojure "1.10.3"]
+ :provided {:dependencies [[org.clojure/clojure "1.11.1"]
                            ;; Logger
                            [com.fzakaria/slf4j-timbre "0.3.21"]
-                           [org.slf4j/slf4j-api "1.7.33"]
-                           [org.slf4j/log4j-over-slf4j "1.7.33"]
-                           [org.slf4j/jul-to-slf4j "1.7.33"]
-                           [org.slf4j/jcl-over-slf4j "1.7.33"]
-                           [ch.qos.logback/logback-classic "1.2.10"
+                           [org.slf4j/slf4j-api "2.0.0"]
+                           [org.slf4j/log4j-over-slf4j "2.0.0"]
+                           [org.slf4j/jul-to-slf4j "2.0.0"]
+                           [org.slf4j/jcl-over-slf4j "2.0.0"]
+                           [ch.qos.logback/logback-classic "1.4.0"
                             :exclusions [org.slf4j/slf4j-api]]
-                           [com.taoensso/timbre "5.1.2"]]}
+                           [com.taoensso/timbre "5.2.1"]]}
  :jar      {:aot :all}}
