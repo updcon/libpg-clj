d743940cb758e86a72544dab08a00521acb1647d
A I <io-devel@copper.mx>
2026-02-04 00:23:10 +0200

    update deps

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,12 @@
 {:dev      {:plugins []}
- :provided {:dependencies [[org.clojure/clojure "1.11.1"]
+ :provided {:dependencies [[org.clojure/clojure "1.12.4"]
                            ;; Logger
-                           [com.fzakaria/slf4j-timbre "0.3.21"]
-                           [org.slf4j/slf4j-api "2.0.0"]
-                           [org.slf4j/log4j-over-slf4j "2.0.0"]
-                           [org.slf4j/jul-to-slf4j "2.0.0"]
-                           [org.slf4j/jcl-over-slf4j "2.0.0"]
-                           [ch.qos.logback/logback-classic "1.4.0"
+                           [com.fzakaria/slf4j-timbre "0.4.1"]
+                           [org.slf4j/slf4j-api "2.0.17"]
+                           [org.slf4j/log4j-over-slf4j "2.0.17"]
+                           [org.slf4j/jul-to-slf4j "2.0.17"]
+                           [org.slf4j/jcl-over-slf4j "2.0.17"]
+                           [ch.qos.logback/logback-classic "1.5.27"
                             :exclusions [org.slf4j/slf4j-api]]
-                           [com.taoensso/timbre "5.2.1"]]}
+                           [com.taoensso/timbre "6.8.0"]]}
  :jar      {:aot :all}}
--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "https://github.com/updcon/libpg-clj"}
   :dependencies [[org.clojure/java.jdbc "0.7.12"]
                  ;; PostgreSQL
-                 [org.postgresql/postgresql "42.5.0"]
+                 [org.postgresql/postgresql "42.7.9"]
                  [nilenso/honeysql-postgres "0.4.112"]
-                 [com.mchange/c3p0 "0.9.5.5"]
+                 [com.mchange/c3p0 "0.11.2"]
                  ;; Data helpers
-                 [danlentz/clj-uuid "0.1.9"]
-                 [cheshire "5.11.0"]
-                 [joda-time "2.11.1"]])
+                 [danlentz/clj-uuid "0.2.0"]
+                 [cheshire "6.1.0"]
+                 [joda-time "2.14.0"]])
